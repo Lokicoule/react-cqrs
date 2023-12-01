fe407c9db0e61c4c955bb5bbfae56aa4408daa79
Loïk Fekkaï <lokicoule@yahoo.fr>
2023-12-01 19:39:12 +0100

    chore(cqrs): remove generated files

--- packages/cqrs/src/lib/cqrs.spec.ts
+++
@@ -1,7 +0,0 @@
-import { cqrs } from './cqrs';
-
-describe('cqrs', () => {
-  it('should work', () => {
-    expect(cqrs()).toEqual('cqrs');
-  });
-});
--- packages/cqrs/src/lib/cqrs.ts
+++
@@ -1,3 +0,0 @@
-export function cqrs(): string {
-  return 'cqrs';
-}
